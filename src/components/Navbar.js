e6d170860b5aa2faeff1a303747c6c805429e8b3
panth1823 <panthshah242@gmail.com>
2024-02-07 14:57:04 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" style={{ boxShadow: ' 0px 7px 10px grey' }}>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
+        style={{ boxShadow: " 0px 7px 10px grey" }}
+      >
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             NexusNews
@@ -23,22 +25,50 @@ const Navbar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/science"> Science</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-              <li className="nav-item"> <Link className="nav-link" to="/technology"> Technology</Link></li>
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  {" "}
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                {" "}
+                <Link className="nav-link" to="/technology">
+                  {" "}
+                  Technology
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
     </div>
-
-
   );
-
-}
+};
 
 export default Navbar;
