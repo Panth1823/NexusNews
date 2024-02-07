d3ce4fde0e7ecebaee01f7858b53a79c61231092
panth1823 <panthshah242@gmail.com>
2024-02-07 14:41:27 +0530

    updates

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ const NewsItem = (props) => {
         <img src={!imageUrl ? "https://medias.liberation.fr/photo/1375474-2021-02-margueritebornhauser-compiegnes-10jpg.jpg?modified_at=1612265502" : imageUrl} className="card-img-top" alt="..." style={{ left: '92%', zIndex: '1', boxShadow: ' 5px -5px 10px grey' }} />
         <div className="card-body" style={{ left: '92%', zIndex: '1', boxShadow: ' 5px 5px 10px grey' }}>
           <h5 className="card-title">{title}...<span
-            class="position-absolute top-0 translate-middle badge rounded-pill bg-primary border border-light rounded-circle"
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-primary border border-light rounded-circle"
             style={{ left: '85%', zIndex: '1' }}>{source}
           </span></h5>
           <p className="card-text">{description}...</p>
