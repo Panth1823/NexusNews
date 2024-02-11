67f8b0bdf3675298f8f4c8aa8544f978c7c32bdb
panth1823 <panthshah242@gmail.com>
2024-02-11 22:52:55 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
 const App = () => {
-  const pageSize = 5;
+  const pageSize = 6;
   const apiKey = process.env.REACT_APP_NEWS_API;
   const [progress, setProgress] = useState(0);
 
